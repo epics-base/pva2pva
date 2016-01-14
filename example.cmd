b1859077af9e9c6ce2ec6990f7d4581066317131
Michael Davidsaver <mdavidsaver@gmail.com>
2016-01-14 12:54:28 -0500

    note available env. config

--- example.cmd
+++ example.cmd
@@ -1,5 +1,6 @@
 # Bind gateway server side to this interface
 epicsEnvSet("EPICS_PVAS_INTF_ADDR_LIST","10.0.1.200")
+
 # Gateway client side searches here.  Must not include gateway server side interface
 epicsEnvSet("EPICS_PVA_ADDR_LIST", "10.1.1.255")
 # Prevent gateway client from automatically populating the address list,
@@ -7,3 +8,19 @@ epicsEnvSet("EPICS_PVA_ADDR_LIST", "10.1.1.255")
 epicsEnvSet("EPICS_PVA_AUTO_ADDR_LIST","NO")
 
 gwstart()
+
+# PVA variables
+#
+# Server side
+#
+# EPICS_PVAS_INTF_ADDR_LIST - Bind to this interface for both UDP and TCP
+# EPICS_PVAS_SERVER_PORT - default TCP port
+# EPICS_PVAS_BROADCAST_PORT - Listen for searches on this port
+#
+# EPICS_PVA_SERVER_PORT - Unused if EPICS_PVAS_SERVER_PORT set
+#
+# Client side
+#
+# EPICS_PVA_BROADCAST_PORT - Default search port for *ADDR_LIST
+# EPICS_PVA_ADDR_LIST - Space seperated list of search endpoints (bcast or unicast)
+# EPICS_PVA_AUTO_ADDR_LIST - YES/NO whether to populate ADDR_LIST with all local interface bcast addrs
