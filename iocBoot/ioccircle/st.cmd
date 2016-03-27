f4d1d5f458d49da8899ba5ba4bb902edb57e2953
Michael Davidsaver <mdavidsaver@gmail.com>
2016-03-28 08:20:02 +0900

    more circle demo

--- iocBoot/ioccircle/st.cmd
+++ iocBoot/ioccircle/st.cmd
@@ -1,5 +1,9 @@
 #!../../bin/linux-x86_64-debug/softIocPVA
 
+epicsEnvSet("EPICS_PVA_ADDR_LIST", "10.5.2.255")
+epicsEnvSet("EPICS_PVAS_INTF_ADDR_LIST","10.5.2.1")
+epicsEnvSet("EPICS_PVA_AUTO_ADDR_LIST","NO")
+
 dbLoadRecords("circle.db","")
 
 iocInit()
--- iocBoot/ioccircle2/st.cmd
+++ iocBoot/ioccircle2/st.cmd
@@ -1,5 +1,9 @@
 #!../../bin/linux-x86_64-debug/softIocPVA
 
+epicsEnvSet("EPICS_PVAS_INTF_ADDR_LIST","10.5.1.1")
+epicsEnvSet("EPICS_PVA_ADDR_LIST", "10.5.1.255")
+epicsEnvSet("EPICS_PVA_AUTO_ADDR_LIST","NO")
+
 dbLoadRecords("circle2.db","")
 
 iocInit()
