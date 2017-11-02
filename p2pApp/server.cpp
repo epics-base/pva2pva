4c8512a88410108eee1d5cb2d02872f9ad2f7cff
Michael Davidsaver <mdavidsaver@gmail.com>
2017-11-02 13:47:11 -0500

    less noise

--- p2pApp/server.cpp
+++ p2pApp/server.cpp
@@ -91,11 +91,9 @@ GWServerChannelProvider::createChannel(std::string const & channelName,
     }
 
     if(!ret) {
-        std::cerr<<"GWServer refusing channel "<<channelName<<"\n";
         pvd::Status S(pvd::Status::STATUSTYPE_ERROR, "Not found");
         channelRequester->channelCreated(S, ret);
     } else {
-        std::cerr<<"GWServer connecting channel "<<channelName<<" as "<<newName<<"\n";
         channelRequester->channelCreated(pvd::Status::Ok, ret);
         channelRequester->channelStateChange(ret, pva::Channel::CONNECTED);
     }
