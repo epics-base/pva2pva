d9816e14fd602f23b7833341e69aa6f2c1e9cfa6
Michael Davidsaver <mdavidsaver@gmail.com>
2017-11-09 16:31:59 -0600

    documentation

---
+++ documentation/qsrvpage.h
@@ -0,0 +1,13 @@
+/**
+@page qsrv_page QSRV
+
+@section qsrv_config QSRV Configuration
+
+By default QSRV exposes all Process Variables (fields of process database records).
+In addition to these "single" PVs, special "group" PVs.
+
+@subsection qsrv_group_sym Group PV semantics
+
+@subsection qsrv_group_def Group PV definitions
+
+*/
