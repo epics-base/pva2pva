3e747809d9c3167936be74c1bf081cb159a3d829
Dirk Zimoch <dirk.zimoch@psi.ch>
2020-04-15 13:12:09 +0200

    removed spaces at end of line

--- pdbApp/softMain.cpp
+++ pdbApp/softMain.cpp
@@ -116,31 +116,31 @@ int main(int argc, char *argv[])
     char xmacro[PVNAME_STRINGSZ + 4];
     int startIocsh = 1; /* default = start shell */
     int loadedDb = 0;
-    
+
     arg0 = strrchr(*argv, '/');
     if (!arg0) {
         arg0 = *argv;
     } else {
         ++arg0; /* skip the '/' */
     }
-    
+
     --argc, ++argv;
-    
+
     /* Do this here in case the dbd file not available */
     if (argc>0 && **argv=='-' && (*argv)[1]=='h') {
         usage(EXIT_SUCCESS);
     }
-    
+
     if (argc>1 && **argv=='-' && (*argv)[1]=='D') {
         dbd_file = *++argv;
         argc -= 2;
         ++argv;
     }
-    
+
     if (dbLoadDatabase(dbd_file, NULL, NULL)) {
         epicsExit(EXIT_FAILURE);
     }
-    
+
     softIocPVA_registerRecordDeviceDriver(pdbbase);
     registryFunctionAdd("exit", (REGISTRYFUNCTION) exitSubroutine);
 
@@ -151,7 +151,7 @@ int main(int argc, char *argv[])
             asSetFilename(*++argv);
             --argc;
             break;
-        
+
         case 'd':
             if (dbLoadRecords(*++argv, macros)) {
                 epicsExit(EXIT_FAILURE);
@@ -159,22 +159,22 @@ int main(int argc, char *argv[])
             loadedDb = 1;
             --argc;
             break;
-        
+
         case 'h':
             usage(EXIT_SUCCESS);
-        
+
         case 'm':
             macros = *++argv;
             --argc;
             break;
-        
+
         case 'S':
             startIocsh = 0;
             break;
-        
+
         case 's':
             break;
-        
+
         case 'x':
             epicsSnprintf(xmacro, sizeof xmacro, "IOC=%s", *++argv);
             if (dbLoadRecords(exit_db, xmacro)) {
@@ -183,7 +183,7 @@ int main(int argc, char *argv[])
             loadedDb = 1;
             --argc;
             break;
-        
+
         default:
             printf("%s: option '%s' not recognized\n", arg0, *argv);
             usage(EXIT_FAILURE);
@@ -191,7 +191,7 @@ int main(int argc, char *argv[])
         --argc;
         ++argv;
     }
-    
+
     if (argc>0 && **argv=='-') {
         switch((*argv)[1]) {
         case 'a':
@@ -200,17 +200,17 @@ int main(int argc, char *argv[])
         case 'x':
             printf("%s: missing argument to option '%s'\n", arg0, *argv);
             usage(EXIT_FAILURE);
-        
+
         case 'h':
             usage(EXIT_SUCCESS);
-        
+
         case 'S':
             startIocsh = 0;
             break;
-        
+
         case 's':
             break;
-        
+
         default:
             printf("%s: option '%s' not recognized\n", arg0, *argv);
             usage(EXIT_FAILURE);
@@ -218,19 +218,19 @@ int main(int argc, char *argv[])
         --argc;
         ++argv;
     }
-    
+
     if (loadedDb) {
         iocInit();
         epicsThreadSleep(0.2);
     }
-    
+
     /* run user's startup script */
     if (argc>0) {
         if (iocsh(*argv)) epicsExit(EXIT_FAILURE);
         epicsThreadSleep(0.2);
         loadedDb = 1;   /* Give it the benefit of the doubt... */
     }
-    
+
     /* start an interactive shell if it was requested */
     if (startIocsh) {
         iocsh(NULL);
