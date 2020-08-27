a47a572b68f47bed9e1d338275a2300f6a27488c
Matic Pogacnik <mpogacnik@anl.gov>
2020-08-27 15:21:08 +0200

    Add AS write hooks for groups

--- pdbApp/pdbgroup.cpp
+++ pdbApp/pdbgroup.cpp
@@ -8,6 +8,7 @@
 #include <dbAccess.h>
 #include <dbChannel.h>
 #include <dbStaticLib.h>
+#include <asLib.h>
 
 #include <pv/pvAccess.h>
 #include <pv/configuration.h>
@@ -349,12 +350,24 @@ void PDBGroupPut::put(pvd::PVStructure::shared_pointer const & value,
     // assume value may be a different struct each time... lot of wasted prep work
     const size_t npvs = channel->pv->members.size();
     std::vector<std::tr1::shared_ptr<PVIF> > putpvif(npvs);
+    std::vector<void *> asWritePvt;
 
     for(size_t i=0; i<npvs; i++)
     {
         PDBGroupPV::Info& info = channel->pv->members[i];
-        if(!info.allowProc) continue;
 
+        asWritePvt.push_back(
+            asTrapWriteWithData(channel->aspvt.at(i).aspvt,
+                            std::string(channel->cred.user.begin(), channel->cred.user.end()).c_str(),
+                            std::string(channel->cred.host.begin(), channel->cred.host.end()).c_str(),
+                            info.chan,
+                            info.chan->final_type,
+                            info.chan->final_no_elements,
+                            NULL
+                            )
+        );
+
+        if(!info.allowProc) continue;
         putpvif[i].reset(info.builder->attach(info.chan, value, info.attachment));
     }
 
@@ -385,6 +398,11 @@ void PDBGroupPut::put(pvd::PVStructure::shared_pointer const & value,
     requester_type::shared_pointer req(requester.lock());
     if(req)
         req->putDone(ret, shared_from_this());
+
+    for(std::vector<void *>::iterator it = asWritePvt.begin();
+            it != asWritePvt.end(); ++it) {
+        asTrapWriteAfter(*it);
+    }
 }
 
 void PDBGroupPut::get()
