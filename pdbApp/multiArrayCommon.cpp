ab03d4ff2a9a991d6d048dcd870f2d3e62a2c5b7
Michael Davidsaver <mdavidsaver@gmail.com>
2020-11-23 19:25:40 -0800

    multiArrayCommon

---
+++ pdbApp/multiArrayCommon.cpp
@@ -0,0 +1,379 @@
+/*************************************************************************\
+* Copyright (c) 2020 Michael Davidsaver
+* EPICS BASE is distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution.
+\*************************************************************************/
+
+#include <list>
+#include <map>
+
+#include <recGbl.h>
+#include <alarm.h>
+#include <errlog.h>
+
+#include <pv/standardField.h>
+
+#define GEN_SIZE_OFFSET
+#include "multiArrayCommon.h"
+#undef  GEN_SIZE_OFFSET
+#include <epicsExport.h>
+
+namespace multiArray {
+
+namespace pvd = epics::pvData;
+
+// used during rset init_record to store column definitions and initial values
+struct EntryStorage {
+    std::string name, label;
+    pvd::ScalarType type;
+    pvd::shared_vector<const void> value;
+};
+
+struct RPvt {
+    bool finished;
+    typedef std::list<EntryStorage> entries_t;
+    entries_t entries;
+    typedef std::map<std::string, EntryStorage*> entries_map_t;
+    entries_map_t entries_map;
+
+    RPvt() :finished(false) {}
+};
+
+static
+void storeAlarm(pvd::PVStructure& root, pvd::BitSet& vld,
+               unsigned short sevr, const char* amsg)
+{
+    pvd::PVScalarPtr fld(root.getSubFieldT<pvd::PVScalar>("alarm.severity"));
+    fld->putFrom<pvd::uint16>(sevr);
+    vld.set(fld->getFieldOffset());
+
+    //TODO: map status properly
+    fld = root.getSubFieldT<pvd::PVScalar>("alarm.status");
+    fld->putFrom<pvd::uint16>(sevr ? 1 : 0);
+    vld.set(fld->getFieldOffset());
+
+    fld = root.getSubFieldT<pvd::PVScalar>("alarm.message");
+    fld->putFrom(std::string(amsg));
+    vld.set(fld->getFieldOffset());
+}
+
+static
+void storeTime(pvd::PVStructure& root, pvd::BitSet& vld,
+               const epicsTimeStamp& time, epicsInt32 utag)
+{
+    pvd::PVScalarPtr fld(root.getSubFieldT<pvd::PVScalar>("timeStamp.secondsPastEpoch"));
+    fld->putFrom<pvd::uint32>(time.secPastEpoch + POSIX_TIME_AT_EPICS_EPOCH);
+    vld.set(fld->getFieldOffset());
+
+    fld = root.getSubFieldT<pvd::PVScalar>("timeStamp.nanoseconds");
+    fld->putFrom<pvd::uint32>(time.nsec);
+    vld.set(fld->getFieldOffset());
+
+    fld = root.getSubFieldT<pvd::PVScalar>("timeStamp.userTag");
+    fld->putFrom<pvd::int32>(utag);
+    vld.set(fld->getFieldOffset());
+}
+
+void add_column(void *pvoid, const char* fname, const char* label, pvd::ScalarType type)
+{
+    multiArrayCommonRecord *prec = (multiArrayCommonRecord*)pvoid;
+
+    if(prec->rpvt->finished)
+        throw std::logic_error("can't add_column() after init_record()");
+
+    if(prec->rpvt->entries_map.find(fname)!=prec->rpvt->entries_map.end())
+        throw std::logic_error("Can't add_column() with duplicate name");
+
+    prec->rpvt->entries.push_back(EntryStorage());
+    EntryStorage& ent = prec->rpvt->entries.back();
+    prec->rpvt->entries_map[fname] = &ent;
+
+    ent.name = fname;
+    ent.label = label ? label : fname;
+    ent.type = type;
+}
+
+void add_columns(void *pvoid, const Entry* cols)
+{
+    multiArrayCommonRecord *prec = (multiArrayCommonRecord*)pvoid;
+
+    for(;cols->name; ++cols) {
+        add_column(prec, cols->name, cols->label, cols->type);
+    }
+}
+
+void set_column(void *pvoid, const char* fname, const pvd::shared_vector<const void>& cdata, const ColMeta* pmeta)
+{
+    multiArrayCommonRecord *prec = (multiArrayCommonRecord*)pvoid;
+
+    RPvt::entries_map_t::iterator it(prec->rpvt->entries_map.find(fname));
+    if(it==prec->rpvt->entries_map.end())
+        throw std::logic_error("No such column");
+
+    EntryStorage& ent = *it->second;
+
+    if(!prec->rpvt->finished) {
+        // save for later during init_record(, 1)
+        ent.value = cdata;
+        // TODO: save meta
+
+    } else {
+        // called from process()
+
+        if(prec->lay==multiArrayLayoutTable) {
+            pvd::PVScalarArrayPtr arr(prec->val->getSubFieldT<pvd::PVScalarArray>(ent.name));
+            arr->putFrom(cdata);
+            prec->vld.set(arr->getFieldOffset());
+
+        } else if(prec->lay==multiArrayLayoutComposite) {
+            pvd::PVStructurePtr base(prec->val->getSubFieldT<pvd::PVStructure>(ent.name));
+
+            pvd::PVScalarArrayPtr arr(base->getSubFieldT<pvd::PVScalarArray>("value"));
+            arr->putFrom(cdata);
+            prec->vld.set(arr->getFieldOffset());
+
+            if(pmeta) {
+                storeAlarm(*base, prec->vld, pmeta->sevr, pmeta->amsg ? pmeta->amsg : "");
+                storeTime(*base, prec->vld, pmeta->time, pmeta->utag);
+            }
+        }
+    }
+}
+
+#define TRY try
+#define CATCH() catch(std::exception& e) { \
+    (void)recGblSetSevrMsg(prec, COMM_ALARM, INVALID_ALARM, "exc: %s", e.what()); \
+    errlogPrintf("%s: unhandled exception: %s\n", prec->name, e.what()); \
+}
+
+
+ELLLIST vfList = ELLLIST_INIT;
+VFieldTypeNode vfNodes[2];
+
+long initialize()
+{
+    vfNodes[0].vtype = &vfStructure;
+    ellAdd(&vfList, &vfNodes[0].node);
+    vfNodes[1].vtype = &vfPVStructure;
+    ellAdd(&vfList, &vfNodes[1].node);
+    return 0;
+}
+
+long init_record(struct dbCommon *pcommon, int pass)
+{
+    multiArrayCommonRecord *prec = (multiArrayCommonRecord*)pcommon;
+    TRY {
+        if(pass==0) {
+            prec->rpvt = new RPvt;
+            new (&prec->val) pvd::PVStructurePtr();
+            new (&prec->vld) pvd::BitSet;
+
+            // caller may now add_column()
+
+            return 0;
+
+        } else if(pass==1) {
+            // caller has made all calls to add_column()
+
+            prec->rpvt->finished = true;
+
+            const pvd::StandardFieldPtr& sfld(pvd::getStandardField());
+
+            pvd::FieldBuilderPtr builder = pvd::FieldBuilder::begin();
+
+            if(prec->lay==multiArrayLayoutTable) {
+
+                pvd::shared_vector<std::string> labels;
+                labels.reserve(prec->rpvt->entries.size());
+
+                builder = builder->setId("epics:nt/NTTable:1.0")
+                        ->addArray("labels", pvd::pvString)
+                        ->addNestedStructure("value");
+
+                for(RPvt::entries_t::const_iterator it(prec->rpvt->entries.begin()),
+                    end(prec->rpvt->entries.end()); it!=end; ++it)
+                {
+                    const EntryStorage& ent = *it;
+
+                    builder = builder->addArray(ent.name, ent.type);
+                    labels.push_back(ent.label);
+                }
+
+                builder = builder->endNested()
+                        ->add("alarm", sfld->alarm())
+                        ->add("timeStamp", sfld->timeStamp());
+
+                prec->val = builder->createStructure()->build();
+
+                pvd::PVStringArrayPtr flabels(prec->val->getSubFieldT<pvd::PVStringArray>("labels"));
+                flabels->replace(pvd::freeze(labels));
+                prec->vld.set(flabels->getFieldOffset());
+
+                pvd::PVStructurePtr value(prec->val->getSubFieldT<pvd::PVStructure>("value"));
+
+                for(RPvt::entries_t::const_iterator it(prec->rpvt->entries.begin()),
+                    end(prec->rpvt->entries.end()); it!=end; ++it)
+                {
+                    const EntryStorage& ent = *it;
+
+                    if(!ent.value.empty())
+                        continue;
+
+                    pvd::PVScalarArrayPtr fld(value->getSubFieldT<pvd::PVScalarArray>(ent.name));
+                    fld->putFrom(ent.value);
+                    prec->vld.set(fld->getFieldOffset());
+                }
+
+            } else if(prec->lay==multiArrayLayoutComposite) {
+
+                for(RPvt::entries_t::const_iterator it(prec->rpvt->entries.begin()),
+                    end(prec->rpvt->entries.end()); it!=end; ++it)
+                {
+                    const EntryStorage& ent = *it;
+
+                    pvd::StructureConstPtr substruct(sfld->scalarArray(ent.type, "alarm,timeStamp"));
+
+                    builder = builder->add(ent.name, substruct);
+                }
+
+                prec->val = builder->createStructure()->build();
+
+                for(RPvt::entries_t::iterator it(prec->rpvt->entries.begin()),
+                    end(prec->rpvt->entries.end()); it!=end; ++it)
+                {
+                    EntryStorage& ent = *it;
+
+                    if(!ent.value.empty())
+                        continue;
+
+                    pvd::PVStructurePtr base(prec->val->getSubFieldT<pvd::PVStructure>(ent.name));
+
+                    pvd::PVScalarArrayPtr fld(base->getSubFieldT<pvd::PVScalarArray>("value"));
+                    fld->putFrom(ent.value);
+                    ent.value = pvd::shared_vector<const void>(); // clear
+                    prec->vld.set(fld->getFieldOffset());
+
+                    // TODO initial alarm/time
+                }
+
+            } else {
+                errlogPrintf("%s.LAY: out of range value %d\n", prec->name, prec->lay);
+                (void)recGblSetSevrMsg(prec, COMM_ALARM, INVALID_ALARM, "Bad LAY %d", prec->lay);
+                return -1;
+            }
+
+            return 0;
+        }
+    }CATCH()
+    return -1;
+}
+
+void monitor(void *pvoid, unsigned short monitor_mask)
+{
+    multiArrayCommonRecord *prec = (multiArrayCommonRecord*)pvoid;
+
+    if(monitor_mask&DBE_ALARM) {
+        // sync record alarm into PVD structure
+        if(prec->lay==multiArrayLayoutTable) {
+            storeAlarm(*prec->val, prec->vld, prec->sevr, prec->amsg);
+
+        } else if(prec->lay==multiArrayLayoutComposite) {
+            // no top level alarm
+        }
+    }
+
+    if(prec->lay==multiArrayLayoutTable) {
+        storeTime(*prec->val, prec->vld, prec->time, prec->utag);
+
+    } else if(prec->lay==multiArrayLayoutComposite) {
+        // no top level timestamp
+    }
+
+    if (monitor_mask) {
+        db_post_events(prec, &prec->val, monitor_mask);
+    }
+}
+
+long cvt_dbaddr(DBADDR *paddr)
+{
+    dbCommon *prec = paddr->precord;
+    TRY {
+        // for VAL
+
+        // we don't provide a valid DBR buffer
+        paddr->ro = 1;
+        // arbitrary limit
+        paddr->no_elements = 1;
+
+        paddr->field_type = DBF_NOACCESS;
+
+        if(dbGetFieldIndex(paddr)==multiArrayCommonRecordVAL) {
+            // we provide vfield access
+            paddr->vfields = &vfList;
+        }
+
+        return 0;
+    }CATCH()
+    return -1;
+}
+
+long get_array_info(DBADDR *paddr, long *no_elements, long *offset)
+{
+    return S_db_badField;
+}
+
+long put_array_info(DBADDR *paddr, long nNew)
+{
+    return S_db_noMod;
+}
+
+long get_vfield(struct dbAddr *paddr, struct VField *p)
+{
+    multiArrayCommonRecord *prec = (multiArrayCommonRecord*)paddr->precord;
+    TRY {
+
+        if(!prec->val)
+            return S_db_notInit;
+
+        if(p->vtype==&vfPVStructure) {
+            VSharedPVStructure *pstr = (VSharedPVStructure*)p;
+            if(dbGetFieldIndex(paddr)==multiArrayCommonRecordVAL) {
+                if(!*pstr->value)
+                    return S_db_notInit;
+                (*pstr->value)->copy(*prec->val);
+                *pstr->changed = prec->vld;
+                return 0;
+            }
+
+        } else if(p->vtype==&vfStructure) {
+            VSharedStructure *pstr = (VSharedStructure*)p;
+            if(dbGetFieldIndex(paddr)==multiArrayCommonRecordVAL) {
+                *pstr->value = prec->val->getStructure();
+                return 0;
+            }
+        }
+        return S_db_badChoice;
+    }CATCH()
+    return -1;
+}
+
+long put_vfield(struct dbAddr *paddr, const struct VField *p)
+{
+    multiArrayCommonRecord *prec = (multiArrayCommonRecord*)paddr->precord;
+    TRY {
+        if(!prec->val)
+            return S_db_notInit;
+
+        if(p->vtype==&vfPVStructure) {
+            const VSharedPVStructure *pstr = (const VSharedPVStructure*)p;
+            if(dbGetFieldIndex(paddr)==multiArrayCommonRecordVAL) {
+                prec->val->copy(**pstr->value);
+                prec->vld |= *pstr->changed;
+            }
+        }
+        return S_db_badChoice;
+    }CATCH()
+    return -1;
+}
+
+} // namespace multiArray
--- pdbApp/statBinRecord.cpp
+++ pdbApp/statBinRecord.cpp
@@ -32,6 +32,8 @@
 #include "pvstructinRecord.h"
 #include "svectorinRecord.h"
 
+#include <multiArrayCommon.h>
+
 #define GEN_SIZE_OFFSET
 #include <statBinRecord.h>
 #undef  GEN_SIZE_OFFSET
@@ -40,19 +42,6 @@
 namespace pvd = epics::pvData;
 
 namespace statBin {
-
-struct RPvt {
-    pvd::PVStructurePtr root;
-    pvd::BitSet valid, changed;
-
-    template<typename PVD>
-    inline
-    std::tr1::shared_ptr<PVD> assign(const char* name) {
-        std::tr1::shared_ptr<PVD> fld(root->getSubFieldT<PVD>(name));
-        changed.set(fld->getFieldOffset());
-        return fld;
-    }
-};
 
 long consume(struct statBinRecord *prec, const epics::pvData::shared_vector<const void>& arr)
 {
@@ -123,11 +112,11 @@ long statBinConsumeIt(struct statBinRecord *prec, const void* raw, size_t nelem)
         std[n] = sqrt(bsum2/cnt - mean[n]*mean[n]);
     }
 
-    prec->rpvt->assign<pvd::PVDoubleArray>("idx.value")->replace(pvd::freeze(idx));
-    prec->rpvt->assign<pvd::PVDoubleArray>("min.value")->replace(pvd::freeze(min));
-    prec->rpvt->assign<pvd::PVDoubleArray>("max.value")->replace(pvd::freeze(max));
-    prec->rpvt->assign<pvd::PVDoubleArray>("mean.value")->replace(pvd::freeze(mean));
-    prec->rpvt->assign<pvd::PVDoubleArray>("std.value")->replace(pvd::freeze(std));
+    multiArray::set_column(prec, "idx", pvd::static_shared_vector_cast<const void>(pvd::freeze(idx)));
+    multiArray::set_column(prec, "min", pvd::static_shared_vector_cast<const void>(pvd::freeze(min)));
+    multiArray::set_column(prec, "max", pvd::static_shared_vector_cast<const void>(pvd::freeze(max)));
+    multiArray::set_column(prec, "mean", pvd::static_shared_vector_cast<const void>(pvd::freeze(mean)));
+    multiArray::set_column(prec, "std", pvd::static_shared_vector_cast<const void>(pvd::freeze(std)));
 
     return -1;
 }
@@ -151,115 +140,51 @@ long statBinConsume(struct statBinRecord *prec, short dbf, const void* arr, size
 namespace  {
 using namespace statBin;
 
-ELLLIST vfPVStructureList = ELLLIST_INIT;
-VFieldTypeNode vfPVStructureNode[2];
-
 long initialize()
 {
-    vfPVStructureNode[0].vtype = &vfStructure;
-    ellAdd(&vfPVStructureList, &vfPVStructureNode[0].node);
-    vfPVStructureNode[1].vtype = &vfPVStructure;
-    ellAdd(&vfPVStructureList, &vfPVStructureNode[1].node);
-    return 0;
+    return multiArray::initialize();
 }
 
 long init_record(struct dbCommon *pcommon, int pass)
 {
     statBinRecord *prec = (statBinRecord*)pcommon;
     statBindset *pdset = (statBindset *)(prec->dset);
-    try {
+    /* overall ordering
+     * pass==0
+     *   - check dset
+     *   - multiArray::init_record(, 0)
+     * pass==1
+     *   - dset::init_record() -- calls add_column()
+     *   - multiArray::init_record(, 1)
+     */
 
-        if (!pdset) {
-            if(pass==0)
-                recGblRecordError(S_dev_noDSET, prec, "statBin: no DSET");
-            return S_dev_noDSET;
-        }
-
-        if(pass==0) {
-            prec->rpvt = new RPvt;
-
-        } else { // pass==1
-
-            const pvd::StandardFieldPtr& sfld(pvd::getStandardField());
-
-            pvd::StructureConstPtr elem(sfld->scalarArray(pvd::pvDouble, "alarm,timeStamp"));
-
-            prec->rpvt->root = pvd::FieldBuilder::begin()
-                    ->add("idx", elem)
-                    ->add("mean", elem)
-                    ->add("std", elem)
-                    ->add("min", elem)
-                    ->add("max", elem)
-                    ->createStructure()->build();
-
-            if(pdset->common.init_record) {
-                long ret = pdset->common.init_record(pcommon);
-                if(ret)
-                    return ret;
-            }
-
-            prec->rpvt->valid = prec->rpvt->changed;
-        }
-
-        return 0;
-    }catch(std::exception& e){
-        fprintf(stderr, "%s: init_record error: %s\n", prec->name, e.what());
-        return -1;
+    if (!pdset && pass==0) {
+        recGblRecordError(S_dev_noDSET, prec, "statBin: no DSET");
+        return S_dev_noDSET;
     }
+
+    if(pass==1 && pdset->common.init_record) {
+        long ret = pdset->common.init_record(pcommon);
+        if(ret)
+            return ret;
+    }
+
+    long status = multiArray::init_record(pcommon, pass);
+
+    return status;
 }
 
 long readValue(statBinRecord *prec,
                statBindset *pdset)
 {
-    prec->rpvt->changed.clear();
-
-    long ret = pdset->read_arr(prec);
-
-    return  ret;
+    return pdset->read_stats(prec);
 }
 
 void monitor(statBinRecord *prec)
 {
     int monitor_mask = recGblResetAlarms(prec);
 
-    monitor_mask |= DBE_VALUE | DBE_LOG;
-
-    prec->rpvt->assign<pvd::PVScalar>("idx.timeStamp.secondsPastEpoch")->putFrom<pvd::uint32>(prec->time.secPastEpoch + POSIX_TIME_AT_EPICS_EPOCH);
-    prec->rpvt->assign<pvd::PVScalar>("min.timeStamp.secondsPastEpoch")->putFrom<pvd::uint32>(prec->time.secPastEpoch + POSIX_TIME_AT_EPICS_EPOCH);
-    prec->rpvt->assign<pvd::PVScalar>("max.timeStamp.secondsPastEpoch")->putFrom<pvd::uint32>(prec->time.secPastEpoch + POSIX_TIME_AT_EPICS_EPOCH);
-    prec->rpvt->assign<pvd::PVScalar>("mean.timeStamp.secondsPastEpoch")->putFrom<pvd::uint32>(prec->time.secPastEpoch + POSIX_TIME_AT_EPICS_EPOCH);
-    prec->rpvt->assign<pvd::PVScalar>("std.timeStamp.secondsPastEpoch")->putFrom<pvd::uint32>(prec->time.secPastEpoch + POSIX_TIME_AT_EPICS_EPOCH);
-
-    prec->rpvt->assign<pvd::PVScalar>("idx.timeStamp.nanoseconds")->putFrom<pvd::uint32>(prec->time.nsec);
-    prec->rpvt->assign<pvd::PVScalar>("min.timeStamp.nanoseconds")->putFrom<pvd::uint32>(prec->time.nsec);
-    prec->rpvt->assign<pvd::PVScalar>("max.timeStamp.nanoseconds")->putFrom<pvd::uint32>(prec->time.nsec);
-    prec->rpvt->assign<pvd::PVScalar>("mean.timeStamp.nanoseconds")->putFrom<pvd::uint32>(prec->time.nsec);
-    prec->rpvt->assign<pvd::PVScalar>("std.timeStamp.nanoseconds")->putFrom<pvd::uint32>(prec->time.nsec);
-
-    prec->rpvt->assign<pvd::PVScalar>("idx.alarm.severity")->putFrom<pvd::uint32>(prec->sevr);
-    prec->rpvt->assign<pvd::PVScalar>("min.alarm.severity")->putFrom<pvd::uint32>(prec->sevr);
-    prec->rpvt->assign<pvd::PVScalar>("max.alarm.severity")->putFrom<pvd::uint32>(prec->sevr);
-    prec->rpvt->assign<pvd::PVScalar>("mean.alarm.severity")->putFrom<pvd::uint32>(prec->sevr);
-    prec->rpvt->assign<pvd::PVScalar>("std.alarm.severity")->putFrom<pvd::uint32>(prec->sevr);
-
-    prec->rpvt->assign<pvd::PVScalar>("idx.alarm.status")->putFrom<pvd::uint32>(prec->stat ? 1 : 0);
-    prec->rpvt->assign<pvd::PVScalar>("min.alarm.status")->putFrom<pvd::uint32>(prec->stat ? 1 : 0);
-    prec->rpvt->assign<pvd::PVScalar>("max.alarm.status")->putFrom<pvd::uint32>(prec->stat ? 1 : 0);
-    prec->rpvt->assign<pvd::PVScalar>("mean.alarm.status")->putFrom<pvd::uint32>(prec->stat ? 1 : 0);
-    prec->rpvt->assign<pvd::PVScalar>("std.alarm.status")->putFrom<pvd::uint32>(prec->stat ? 1 : 0);
-
-    std::string amsg(prec->amsg);
-    prec->rpvt->assign<pvd::PVString>("idx.alarm.message")->put(amsg);
-    prec->rpvt->assign<pvd::PVString>("min.alarm.message")->put(amsg);
-    prec->rpvt->assign<pvd::PVString>("max.alarm.message")->put(amsg);
-    prec->rpvt->assign<pvd::PVString>("mean.alarm.message")->put(amsg);
-    prec->rpvt->assign<pvd::PVString>("std.alarm.message")->put(amsg);
-
-    if (monitor_mask) {
-        db_post_events(prec, &prec->val, monitor_mask);
-    }
-
-    prec->rpvt->valid |= prec->rpvt->changed;
+    multiArray::monitor(prec, monitor_mask);
 }
 
 long process(struct dbCommon *pcommon)
@@ -271,13 +196,13 @@ long process(struct dbCommon *pcommon)
         unsigned char    pact=prec->pact;
         long status;
 
-        if( (pdset==NULL) || (pdset->read_arr==NULL) ) {
+        if( (pdset==NULL) || (pdset->read_stats==NULL) ) {
             prec->pact=TRUE;
-            recGblSetSevrMsg(prec, READ_ALARM, INVALID_ALARM, "no read_arr");
-            recGblRecordError(S_dev_missingSup,(void *)prec,"read_arr");
+            recGblSetSevrMsg(prec, READ_ALARM, INVALID_ALARM, "no read_stats");
+            recGblRecordError(S_dev_missingSup,(void *)prec,"read_stats");
             return S_dev_missingSup;
 
-        } else if(!prec->rpvt->root) {
+        } else if(!prec->val) {
             prec->pact=TRUE;
             recGblSetSevrMsg(prec, READ_ALARM, INVALID_ALARM, "no table");
             return S_dev_NoInit;
@@ -305,21 +230,7 @@ long process(struct dbCommon *pcommon)
 
 long cvt_dbaddr(DBADDR *paddr)
 {
-    // for VAL
-
-    // we don't provide a valid DBR buffer
-    paddr->ro = 1;
-    // arbitrary limit
-    paddr->no_elements = 1;
-
-    paddr->field_type = DBF_NOACCESS;
-
-    if(dbGetFieldIndex(paddr)==pvstructinRecordVAL) {
-        // we provide vfield access
-        paddr->vfields = &vfPVStructureList;
-    }
-
-    return 0;
+    return multiArray::cvt_dbaddr(paddr);
 }
 
 long get_array_info(DBADDR *paddr, long *no_elements, long *offset)
@@ -334,29 +245,7 @@ long put_array_info(DBADDR *paddr, long nNew)
 
 long get_vfield(struct dbAddr *paddr, struct VField *p)
 {
-    statBinRecord *prec = (statBinRecord*)paddr->precord;
-
-    if(!prec->rpvt->root)
-        return S_db_notInit;
-
-    if(p->vtype==&vfPVStructure) {
-        VSharedPVStructure *pstr = (VSharedPVStructure*)p;
-        if(dbGetFieldIndex(paddr)==pvstructinRecordVAL) {
-            if(!*pstr->value)
-                return S_db_notInit;
-            (*pstr->value)->copy(*prec->rpvt->root);
-            *pstr->changed = prec->rpvt->valid; // TODO: distinguish initial from update
-            return 0;
-        }
-
-    } else if(p->vtype==&vfStructure) {
-        VSharedStructure *pstr = (VSharedStructure*)p;
-        if(dbGetFieldIndex(paddr)==pvstructinRecordVAL) {
-            *pstr->value = prec->rpvt->root->getStructure();
-            return 0;
-        }
-    }
-    return S_db_badChoice;
+    return multiArray::get_vfield(paddr, p);
 }
 
 long put_vfield(struct dbAddr *paddr, const struct VField *p)
@@ -399,9 +288,10 @@ rset statBinRSET={
     put_vfield,
 };
 
+// Soft Channel (default) device support
+
 long readLocked(struct link *pinp, void *raw)
 {
-    const bool* doload = static_cast<const bool*>(raw);
     statBinRecord *prec = (statBinRecord *) pinp->precord;
 
     pvd::shared_vector<const void> arr;
@@ -410,10 +300,51 @@ long readLocked(struct link *pinp, void *raw)
     ival.vtype = &vfSharedVector;
     ival.value = &arr;
 
-    long status = *doload ? dbLoadLink(pinp, DBR_VFIELD, &ival) : dbGetLink(pinp, DBR_VFIELD, &ival, 0, 0);
+    long status = dbGetLink(pinp, DBR_VFIELD, &ival, 0, 0);
 
-    if (status==S_db_badDbrtype) {
-        // TODO: fallback to alloc and copy
+    if (status==S_db_badDbrtype && !dbLinkIsConstant(pinp)) {
+        // fallback to reading (and copying) DBF array
+        status=0;
+
+        int dbf = dbGetLinkDBFtype(pinp);
+        pvd::ScalarType stype;
+
+        switch(dbf) {
+#define CASE_SKIP_BOOL
+#define CASE_REAL_INT64
+#define CASE(BASETYPE, PVATYPE, DBFTYPE, PVACODE) case DBF_##DBFTYPE: stype = pvd::pv##PVACODE; break;
+#include <pv/typemap.h>
+#undef CASE
+#undef CASE_SKIP_BOOL
+#undef CASE_REAL_INT64
+        default:
+            // include DBF_STRING
+            recGblSetSevrMsg(prec, READ_ALARM, INVALID_ALARM, "Bad Img DBF %d", dbf);
+            return S_db_badDbrtype;
+        }
+
+        long nelem = 0;
+
+        if(dbf==-1 || (status=dbGetNelements(pinp, &nelem))) {
+            return status;
+        }
+
+        size_t esize = dbValueSize(dbf);
+        try {
+            pvd::shared_vector<char> temp(nelem*esize);
+
+            status = dbGetLink(pinp, dbf, temp.data(), 0, &nelem);
+
+            if(!status) {
+                temp.resize(nelem*esize);
+                arr = pvd::static_shared_vector_cast<const void>(pvd::freeze(temp));
+                arr.set_original_type(stype);
+            }
+        } catch(std::exception& e) {
+            errlogPrintf("%s: Error during alloc/copy of DBF -> PVD: %s\n", prec->name, e.what());
+            recGblSetSevrMsg(prec, READ_ALARM, INVALID_ALARM, "DBF CPY %s", e.what());
+            status = S_rec_outMem; /* this is likely an allocation error */
+        }
     }
 
     if (!status)
@@ -428,24 +359,30 @@ long readLocked(struct link *pinp, void *raw)
     return status;
 }
 
+multiArray::Entry columns[] = {
+    {"idx", "1st Index", pvd::pvDouble},
+    {"min", "Min", pvd::pvDouble},
+    {"max", "Max", pvd::pvDouble},
+    {"mean","Mean", pvd::pvDouble},
+    {"std", "StdDev", pvd::pvDouble},
+    {0},
+};
+
 long init_record_soft(struct dbCommon *pcommon)
 {
     statBinRecord *prec = (statBinRecord *)pcommon;
-    bool doload = true;
 
-    if (readLocked(&prec->inp, &doload)) {
-        prec->udf = FALSE;
-    }
+    multiArray::add_columns(prec, columns);
+
     return 0;
 }
 
 long read_arr_soft(statBinRecord* prec)
 {
-    bool doload = false;
-    long status = dbLinkDoLocked(&prec->inp, readLocked, &doload);
+    long status = dbLinkDoLocked(&prec->inp, readLocked, 0);
 
     if (status == S_db_noLSET)
-        status = readLocked(&prec->inp, &doload);
+        status = readLocked(&prec->inp, 0);
 
     if (!status && !dbLinkIsConstant(&prec->inp))
         prec->udf = FALSE;
